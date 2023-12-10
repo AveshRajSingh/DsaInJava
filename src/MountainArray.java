22259e12a159f33fd5dd69764e455693942a5f35
AveshRajSingh <aveshrajsingh@gmail.com>
2023-12-10 22:44:00 +0530

    this is an problem of array

---
+++ src/MountainArray.java
@@ -0,0 +1,21 @@
+public class MountainArray {
+    public static void main(String[] args) {
+        int []arr = {0,6,7,8,5,6,3,22};
+        boolean result = validMountainArray(arr);
+        System.out.println(result);
+
+    }
+    static boolean validMountainArray(int [] arr){
+        int A = arr.length , i = 0, j = A -1;
+
+        while(arr[i]<arr[i+1]&& i+1<A){
+            i++;
+        }
+        while(j > 0 && arr[j-1]>arr[j]){
+            j--;
+        }
+
+
+        return ( i > 0 && j < A - 1 && i == j);
+    }
+}
